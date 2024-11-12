eca7ee1ec76077c6baf2a6c1758cacbbdc40205e
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-11-12 21:01:18 +0530

    Initial Commit

---
+++ Intagram Fake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Intagram Fake
+//
+//  Created by Akshay kumar shaw on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Intagram Fake/Intagram_FakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intagram_FakeApp.swift
+//  Intagram Fake
+//
+//  Created by Akshay kumar shaw on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Intagram_FakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
